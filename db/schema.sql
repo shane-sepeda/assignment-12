dcd2bf2655576cf3c7d2f5e9aa631e57a5ac906d
shane-sepeda <shane.sepeda@icloud.com>
2022-08-22 01:09:23 -0500

    Updating SQL

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,14 @@
 DROP DATABASE IF EXISTS tracker;
 CREATE DATABASE tracker;
+
+USE tracker;
+
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE department ();
+
+CREATE TABLE role ();
+
+CREATE TABLE employee ();
