ec1a250079886e2989c213974173c34992e67382
shane-sepeda <shane.sepeda@icloud.com>
2022-08-23 22:45:50 -0500

    updating queries.sql

---
+++ db/queries.sql
@@ -0,0 +1,6 @@
+let view_department=`SELECT department AS department_id FROM department`
+let view_role=`SELECT role AS role.id FROM department`
+let view_emps=`SELECT employee AS employee from department`
+let add_dept=`INSERT INTO department (name)`
+let add_emp=`INSERT INTO roles`
+let update_emp=`UPDATE employee SET role.id`
