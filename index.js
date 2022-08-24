f7c8502697688d68a1f2cc1cfaeff85c089aaa0f
shane-sepeda <shane.sepeda@icloud.com>
2022-08-23 22:59:43 -0500

    Updating index.js

--- index.js
+++ index.js
@@ -11,3 +11,25 @@ const menuOption=() {
             choices: ['[1] Departments', '[2] Roles', '[3] Employees', '[4] Exit']
         })
 }
+
+const view_department = () {
+    const view_department=`SELECT department AS department_id FROM department`
+};
+
+const view_role = () {
+    const view_role=`SELECT role AS role.id FROM department`
+};
+
+const view_emps = () {
+    const view_emps=`SELECT department AS department_id FROM department`
+};
+
+const add_dept = () {
+    const add_dept=`SELECT employee AS employee from department`
+};
+
+const add_emp = () {
+    const view_department=`INSERT INTO roles`
+};
+
+initializeApp();
