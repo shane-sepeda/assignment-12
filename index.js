5f283a8eb58748ecae1a7c625b99fff3a357e882
shane-sepeda <shane.sepeda@icloud.com>
2022-08-23 22:48:31 -0500

    updating index.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const inquirer = require("inquirer");
 const db = require("./db/connection");
+require("console-table");
+
+const menuOption=() {
+    inquirer.prompt(
+        {
+            type: "list",
+            name: "menu",
+            message: "Choose Options:",
+            choices: ['[1] Departments', '[2] Roles', '[3] Employees', '[4] Exit']
+        })
+}
