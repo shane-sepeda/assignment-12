ed4ac5c6e9732ed2039490304195674f57e0a6d9
shane-sepeda <shane.sepeda@icloud.com>
2022-08-23 22:40:46 -0500

    updating seeds.SQL

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (name)
+VALUES ("Sales"),("Engineering"),("Finance"),("Legal");
+
+INSERT INTO roles (title, department_id)
+VALUES ("Software Engineer", 1),("Account Manager", 2),("Lawyer", 3);
+
+INSERT INTO employee (first_name, last_name, department_id);
+VALUES ("Mike Chan"),("Ashley Rodriguez"),("Kevin Tupik");
